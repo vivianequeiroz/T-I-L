3c4a386f00a9f27902c516501e76d7f6788bfa25
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2020-10-28 15:17:49 -0300

    chore: if else and loops

--- adivinhacao/adivinhacao-alura-c/adivinhacao.c
+++ adivinhacao/adivinhacao-alura-c/adivinhacao.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define NUMBER_OF_ATTEMPTS 5     
 
 int main() {
  
@@ -6,30 +7,38 @@ int main() {
     printf("*Bem-vindo ao nosso jogo de advinhação!*\n");
     printf("****************************************\n");
     
-    int secretNumber = 42, guess, i;
+    int secretNumber = 42;
+    int guess;
+    int attempts = 1;
     //i representa o --contador--
-    for(i = 1; i <= 3; i++) {          
-
-    printf("Digite aqui o seu chute: \n");
-    scanf("%i", &guess);
+    while(1){
+         printf("Tentativa %i\n", attempts);
+         printf("Qual é o seu chute? ");
+         scanf ("%i", &guess);
+         printf("Seu chute foi %i \n", attempts);
     
-    printf("Tentativa de %i de 3\n", i);
-    printf("O seu chute foi: %i \n", guess);
 
-    int hit = (guess == 42);
+    if (guess < 0){
+         printf("\n***** Ei, você digitou um número negativo, mas o seu chute só pode conter números positivos! *****\n");
+         continue;
+    }
+
+    int hit = guess == secretNumber;
+    int bigger = guess > secretNumber;
 
     if (hit) {
-         printf("Parabéns! Você é top e conseguiu acertar o número secreto c:");
+         printf("Parabéns! Você é top e conseguiu acertar o número secreto c:\n");
          break;
-
-    } else {
-         int bigger = (guess > 42);
-         if (bigger) {
+          
+    } else if (bigger) {
              printf("Poxa, o número que você digitou é maior que o número secreto. \nContinue tentando!\n");    
          } else {
               printf("Poxa, o número que você digitou é menor que o número secreto. \nContinue tentando!\n");    
          }
-     }
-  }
-  printf("Fim de jogo! ;D");
-} 
+
+         attempts++;
+    }
+  
+  printf("Fim de jogo! ;D\n");
+     printf("Voce acertou depois de %i tentativas!!!", attempts);
+}
