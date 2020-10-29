d16d7b29f18a179ca7b432851a00005386a0df40
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2020-10-29 17:44:04 -0300

    feat: ask user a letter to test

--- jogo-forca.c/jogo-forca.c
+++ jogo-forca.c/jogo-forca.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
 
@@ -10,10 +11,19 @@ int main() {
      int hit = 0;
      int hanged = 0;
 
+
      do {
+
+          char guess;
+     printf("Digite uma letra: ");
+     scanf("%c", &guess);
+
+         for(int i= 0; i < strlen(secretWord); i++) {
+             if(secretWord[i] == guess) {
+                 printf("A letra está na posição %i \n", i);
+             }
+         }
          
-     } while (!acertou  == 0 && enforcou == 0);
-
-
+     } while (!hit  == 0 && !hanged == 0);
 
 }
