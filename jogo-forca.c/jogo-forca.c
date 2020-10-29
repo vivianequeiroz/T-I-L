42ab89f7c1fbc91f4116bb93fd3db55ca515b53d
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2020-10-29 11:52:41 -0300

    chore: begin of hangman game

---
+++ jogo-forca.c/jogo-forca.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    char secretWord[20];
+
+    sprint(secretWord, "MELANCIA");
+     
+     int hit = 0;
+     int hanged = 0;
+
+     do {
+         
+     } while (!acertou == 0 && enforcou == 0);
+
+
+
+}
