4351def771ca30315c8d2d33754d10e9c619a466
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2020-10-30 23:33:11 -0300

    chore: pointers and more functions

--- jogo-forca.c/jogo-forca.c
+++ jogo-forca.c/jogo-forca.c
@@ -6,27 +6,83 @@
 void openingGame() {
 
     printf("*********************\n");
-    printf("*** Jogo de Forca ***\n");
+    printf("*** Jogo da Forca ***\n");
     printf("*********************\n\n");
 }
 
-void makeAGuess(char guesses[26], int attempts) {
+
+void makeAGuess(char guesses[26], int *attempts) {
 
     char guess;
     scanf(" %c", &guess);
 
-    guesses[attempts] = guess;
+    guesses[(*attempts)] = guess;
+    (*attempts)++;
 }
 
+int alreadyMadeGuess(char word, char guesses[26], int attempts) {
+    int found = 0;
+
+    for(int j = 0; j < attempts; j++) {
+        if(guesses[j] == word) {
+            found = 1;
+            break;
+        }
+    }
+
+    return found;
+}
+
+void drawGallows(char secretWord[20], char guesses[26], int attempts) {
+
+    for(int i =0; i < strlen(secretWord); i++) {
+            
+        int found = alreadyMadeGuess(secretWord[i], guesses, attempts);
+
+        if(found) {
+            printf("%c", secretWord[i]);
+        } else {
+            printf(" _ ");
+        }
+    }
+    printf("\n");
+}
+
+
+void chooseWord(char secretWord[20]) {
+
+    sprintf(secretWord, "MELANCIA");
+}
+
+int hanged(int  attempts, char secretWord[20], char guesses[26]) {
+
+    int errors = 0;
+    
+    for(int i=0; i < attempts; i++) {
+        
+        int exist = 0;
+        for(int j = 0; j < strlen(secretWord); j++) {
+            if(guesses[i] == secretWord[j]) {
+                
+                exist = 1;
+                break;
+            }
+        }
+
+        if(!exist) errors++;
+    }
+
+    return errors >=5;
+}
 
 int main() {
 
     char secretWord[20];
 
-    sprintf(secretWord, "MELANCIA");
+    chooseWord(secretWord);
      
      int hit = 0;
-     int hanged = 0;
+     int hangedPlayer = 0;
 
      char guesses[26];
      int attempts = 0;
@@ -34,27 +90,11 @@ int main() {
      openingGame();
 
      do {
-        for(int i =0; i < strlen(secretWord); i++) {
-            
-            int find = 0;
+        
+    drawGallows(secretWord, guesses, attempts);
+    makeAGuess(guesses, &attempts);
 
-            for(int j = 0; j < attempts; j++) {
-                if(guesses[j] == secretWord[i]) {
-                    find = 1;
-                    break;
-                }
-            }
+    hangedPlayer = hanged(attempts, secretWord, guesses);
 
-        if(find) {
-            printf("%c", secretWord[i]);
-        } else {
-            printf(" _ ");
-        }
-    }
-    printf("\n");
-
-    makeAGuess(guesses, attempts);
-    attempts++;
-
-    } while (!hit  && !hanged );
+    } while (!hit  && !hangedPlayer);
 }
