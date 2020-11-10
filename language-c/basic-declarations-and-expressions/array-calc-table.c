c38d5184d34e85eb61b263f2700a70d6ddb51507
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2020-11-09 22:08:18 -0300

    fix: table calculator

--- language-c/basic-declarations-and-expressions/array-calc-table.c
+++ language-c/basic-declarations-and-expressions/array-calc-table.c
@@ -5,11 +5,16 @@ int main() {
     int multiplicator[9], multiplying, result;
     int i;
     
-    multiplying = 9;
+    printf("Digite aqui o valor da tabuada que deseja calcular: \n");
+    scanf("%d", &multiplying);
+    
+    for(i = 0; i < 10; i++) {
+         multiplicator[i] = i;
+     }
 
     for(i = 0; i < 10; i++) {
         result = multiplicator[i] * multiplying;
-        printf("O valor de %d por %d e igual a: %d \n", multiplicator, multiplying, result);
+        printf("O valor de %d por %d e igual a: %d \n", multiplicator[i], i, result);
     }
 
 }
