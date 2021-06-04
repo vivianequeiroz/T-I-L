fd695013abb2d2a77e6c8632fc5792b3fb5f7a0d
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-06-03 21:36:26 -0300

    chore: creating table and user with permissions

--- mysql/creating_table.sql
+++ mysql/creating_table.sql
@@ -12,7 +12,7 @@ CREATE TABLE COURSE (
 
 -- DEPENDENT TABLE
 CREATE TABLE STUDENT (
-	enrollmentNumber int,
+	  enrollmentNumber int,
     studentName varchar(100),
     phone varchar(11),
     courseId int NOT NULL,
@@ -20,9 +20,14 @@ CREATE TABLE STUDENT (
     FOREIGN KEY (courseId) REFERENCES COURSE(courseId)
 );
 
--- INSERTING ITEMS INTO A TABLE
+-- INSERTING ITEMS - INDEPENDENT TABLE
 INSERT INTO COURSE (courseId, courseName)
     VALUES
     (01, 'Biology'),
     (02, 'Geology'),
     (03, 'History');
+
+-- INSERTING ITEMS - DEPENDENT TABLE
+INSERT INTO STUDENT(enrollmentNumber, studentName, phone, courseId)
+    VALUES
+    (123, 'João Vitor', '11123456789', 01);
