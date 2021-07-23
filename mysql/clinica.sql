7054cda3108a95dbb78d36a1af8bf7db493017e7
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-23 16:13:22 -0300

    chore: mysql trigger and view exercises

--- mysql/clinica.sql
+++ mysql/clinica.sql
@@ -34,3 +34,29 @@ VALUES
     (2, 'Paulo', 24, 'Palhoca', '20000220000', 'fratura' ),
     (3, 'Lucia', 30, 'Biguacu', '22000200000', 'tendinite' ),
     (4, 'Carlos', 28, 'Joinville', '11000110000', 'sarampo' );
+
+
+
+INSERT INTO tarefa 
+  ( titulo, descricao, tag, data, hora, usuario_id )
+VALUES
+  ( 'Resolver atividades da faculdade', 'Esse grupo de subtarefas tem como objetivo o fechamento das atividades da faculdade', 'FACULDADE', '2021-08-10', '20:00', 1)
+
+
+  ('Agendar oftalmologista para Joao', 'As subtarefas contem o que precisa ser pesquisado antes de agendar uma consulta', 'SAÚDE', '2021-07-30', '17:30', 2),
+  ('Comprar novos talheres e pratos', 'As subtarefas contem as caracteristicas e quantidades de cada talher e prato', 'MERCADO', '2021-08-15', '16:00', 2), 
+
+  ('Inscrição na aula de box', 'Verificar nas subtarefas os preparativos necessarios para a inscricao para aulas de box', 'SAÚDE', '2021-09-18', '10:00', 4),
+  ('Construir uma aplicacao utilizando firebase', 'As subtarefas contem a ideia do projeto e os servicos que serao utilizados', 'ESTUDO', '2021-08-20', '15:00', 4),
+
+  INSERT INTO subtarefa 
+  ( descricao, usuario_id )
+VALUES 
+  ('Pesquisar as clinicas mais proximas e preco das consultas', 2),
+  ('Comprar armacao do jeito', 2)
+
+
+  INSERT INTO subtarefa_tarefa
+  ( usuario_id, tarefa_id, subtarefa_id ) 
+VALUES
+  ( 5,  6, 1 );
