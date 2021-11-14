55f410f648f8272e41c70b154809ed960ba18da4
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-11-14 17:21:54 -0300

    chore: queues general characteristics and behaviour

---
+++ data-structures-and-algorithms/Queue.java
@@ -0,0 +1,48 @@
+import java.utils.Queue;
+import java.utils.LinkedList;
+
+public class Queue {
+  public static void main(String[] args) {
+
+    // Queue<String> queue = new Queue<String>(); it doesn't work because Queue is a
+    // Java Intarface (part of Collection class), so its necessary to use a class
+    // that implements its interface instead (LinkedinList or PriorityQueue) to be
+    // instantiated
+
+    Queue<String> queue = new LinkedList<String>();
+
+    queue.offer("Karen");
+    queue.offer("Chad");
+    queue.offer("Harold");
+
+    System.out.println(queue.peek());
+
+    queue.poll();
+    queue.poll();
+    queue.poll();
+
+    System.out.println(queue.isEmpty());
+    System.out.println(queue.size());
+    System.out.prinln(queue.contains("Harold")); // does not return the position/index of the object
+
+    // ~ Usage examples:
+    // 1 - Keyboard buffer
+
+    // 2 - Printer queue
+
+    // 3 - Used in LinkedLists, PriorityQueues, Breadth-first seach (BFS);
+
+    // ~ General characteristics
+    // FIFO => First In, First Out
+    // Linear Data Structure
+    // A collection designed for >>> holding <<< elements prior to processing
+    // enqueue means addition to a queue, how it works depends on the language (in
+    // Java, it works with offer() )
+    // dequeue means removing from a queue, how it works depends on the language (in
+    // Java, it works with poll() )
+    // Java documentation recommends using the methods that does not throw
+    // exceptions when there is not value:
+    // => offer(), poll() and peek() instead of add(), remove() and element()
+
+  }
+}
