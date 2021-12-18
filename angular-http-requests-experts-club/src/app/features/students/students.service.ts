82dedab10ca80c1a2342ab474c6abd77755ad59b
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-12-18 16:51:14 -0300

    refactor: adapt students service to consume api data

--- angular-http-requests-experts-club/src/app/features/students/students.service.ts
+++ angular-http-requests-experts-club/src/app/features/students/students.service.ts
@@ -1,58 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { Student } from './student';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StudentsService {
-
-  students: Student[] = [];
-
-  constructor() {
-    this.loadStudents();
-  }
+  apiUrl = `${environment.apiUrl}/students`;
+  constructor(private http: HttpClient) {}
 
   save(student: Student) {
-    student.id = this.nextId();
-    this.students.push(student);
+    return this.http.post<Student[]>(this.apiUrl, student);
   }
 
   update(id: number, student: Student) {
-    const index = this.students.findIndex(s => s.id == id);
-    this.students.splice(index, 1, student);
+    return this.http.put<Student[]>(`this.apiUrl/${id}`, student);
   }
 
   deleteById(id: number) {
-    const index = this.students.findIndex(s => s.id == id);
-    this.students.splice(index, 1);
+    return this.http.delete<void>(`this.apiUrl/${id}`);
   }
 
   findById(id: number) {
-    return this.students.find(s => s.id == id);
+    return this.http.get<Student[]>(`this.apiUrl/${id}`);
   }
 
   findAll() {
-    return this.students;
+    return this.http.get<Student[]>(this.apiUrl);
   }
-
-  private nextId() {
-    let lastId = 0;
-    if (this.students.length > 0) {
-      lastId = Math.max(...this.students.map(student => student.id));
-    }
-
-    return ++lastId;
-  }
-
-  private loadStudents() {
-    this.students.push(new Student(1, 'Ana', 'ana@gmail.com', '2001-04-01'));
-    this.students.push(new Student(2, 'Pedro', 'pedro@gmail.com', '2000-08-28'));
-    this.students.push(new Student(3, 'Marcia', 'marcia@gmail.com', '2002-08-21'));
-    this.students.push(new Student(4, 'Marcos', 'marcos@gmail.com', '2003-05-17'));
-    this.students.push(new Student(5, 'Victor', 'victor@gmail.com', '1993-04-12'));
-    this.students.push(new Student(6, 'Vanessa', 'vanessa@gmail.com', '2003-08-09'));
-    this.students.push(new Student(7, 'Mariana', 'mariana@gmail.com', '2000-11-06'));
-    this.students.push(new Student(8, 'Bernardo', 'bernardo@gmail.com', '1995-02-27'));
-  }
-
 }
