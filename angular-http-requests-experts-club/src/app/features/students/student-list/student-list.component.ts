50f0e32f5623ff73339e44368154c7d121383143
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-12-18 16:54:39 -0300

    refactor: adapt students list component to delete and load students using subscribe

--- angular-http-requests-experts-club/src/app/features/students/student-list/student-list.component.ts
+++ angular-http-requests-experts-club/src/app/features/students/student-list/student-list.component.ts
@@ -5,24 +5,26 @@ import { StudentsService } from '../students.service';
 @Component({
   selector: 'app-student-list',
   templateUrl: './student-list.component.html',
-  styleUrls: ['./student-list.component.css']
+  styleUrls: ['./student-list.component.css'],
 })
 export class StudentListComponent implements OnInit {
-
   students: Student[] = [];
 
-  constructor(private studentService: StudentsService) { }
+  constructor(private studentService: StudentsService) {}
 
   ngOnInit(): void {
     this.loadStudents();
   }
 
   onDelete(id: number) {
-    this.studentService.deleteById(id);
+    this.studentService.deleteById(id).subscribe(() => {
+      this.loadStudents();
+    });
   }
 
   private loadStudents() {
-    this.students = this.studentService.findAll();
+    this.studentService.findAll().subscribe((response) => {
+      this.students = response;
+    });
   }
-
 }
