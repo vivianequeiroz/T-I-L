def5af8eeb923e4c647746111d5d129f7109d641
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-12-18 15:28:39 -0300

    refactor: remove the logic from other services and make books service have a single responsability

--- angular-interceptors-experts-club/books-list/src/app/services/books.service.ts
+++ angular-interceptors-experts-club/books-list/src/app/services/books.service.ts
@@ -1,17 +1,7 @@
-import {
-  HttpClient,
-  HttpErrorResponse,
-  HttpHeaders,
-} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { Observable, throwError } from 'rxjs';
-import { catchError, delay, finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { AlertService } from './alert.service';
-import { AuthService } from './auth.service';
-import { LoaderService } from './loader.service';
-import { DialogData } from './../models/DialogData';
 
 export interface Book {
   id: number;
@@ -25,101 +15,25 @@ export interface Book {
 export class BooksService {
   private baseUrl = environment.apiUrl;
 
-  constructor(
-    private http: HttpClient,
-    private auth: AuthService,
-    private loader: LoaderService,
-    private router: Router,
-    private alert: AlertService
-  ) {}
+  constructor(private http: HttpClient) {}
 
   getAllBooks(): Observable<Book[]> {
-    this.loader.startLoading();
-    const headers: HttpHeaders = this.getAuthorizationHeader();
-
-    return this.http
-      .get<Book[]>(`${this.baseUrl}/books`, {
-        headers,
-      })
-      .pipe(
-        delay(3000), //useful to test slowness
-        catchError((error) => this.handleError(error)),
-        finalize(() => this.loader.stopLoading())
-      );
+    return this.http.get<Book[]>(`${this.baseUrl}/books`);
   }
 
   getBookById(bookId: number): Observable<Book> {
-    this.loader.startLoading();
-    const headers: HttpHeaders = this.getAuthorizationHeader();
-
-    return this.http
-      .get<Book>(`${this.baseUrl}/books/${bookId}`, {
-        headers,
-      })
-      .pipe(
-        delay(3000),
-        finalize(() => this.loader.stopLoading())
-      );
+    return this.http.get<Book>(`${this.baseUrl}/books/${bookId}`);
   }
 
   createBook(book: Omit<Book, 'id'>): Observable<Book> {
-    this.loader.startLoading();
-    const headers: HttpHeaders = this.getAuthorizationHeader();
-
-    return this.http
-      .post<Book>(`${this.baseUrl}/books`, book, {
-        headers,
-      })
-      .pipe(
-        delay(3000),
-        finalize(() => this.loader.stopLoading())
-      );
+    return this.http.post<Book>(`${this.baseUrl}/books`, book);
   }
 
   updateBook(book: Book): Observable<Book> {
-    const headers: HttpHeaders = this.getAuthorizationHeader();
-    return this.http
-      .put<Book>(`${this.baseUrl}/books/${book.id}`, book, {
-        headers,
-      })
-      .pipe(
-        delay(3000),
-        finalize(() => this.loader.stopLoading())
-      );
+    return this.http.put<Book>(`${this.baseUrl}/books/${book.id}`, book);
   }
 
   deleteBookById(bookId: number): Observable<Book> {
-    this.loader.startLoading();
-    const headers: HttpHeaders = this.getAuthorizationHeader();
-
-    return this.http
-      .delete<Book>(`${this.baseUrl}/books/${bookId}`, {
-        headers,
-      })
-      .pipe(
-        delay(3000),
-        finalize(() => this.loader.stopLoading())
-      );
-  }
-
-  private getAuthorizationHeader(): HttpHeaders {
-    return new HttpHeaders({
-      authorization: `Bearer ${this.auth.getAccessToken()}`,
-    });
-  }
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 401) {
-      const dialogData: DialogData = {
-        status: error.status,
-        statusText: error.statusText,
-        message: 'You must be logged in to view page.',
-      };
-      this.alert
-        .showError(dialogData)
-        .subscribe(() => this.router.navigate(['login']));
-    }
-
-    return throwError(error);
+    return this.http.delete<Book>(`${this.baseUrl}/books/${bookId}`);
   }
 }
