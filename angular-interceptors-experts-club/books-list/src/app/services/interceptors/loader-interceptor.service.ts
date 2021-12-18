7d55dba6901c8c6230dadb7dda5c63e0582287a3
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-12-18 15:24:57 -0300

    feat: add logic only related to the loader inteceptor into the service

--- angular-interceptors-experts-club/books-list/src/app/services/interceptors/loader-interceptor.service.ts
+++ angular-interceptors-experts-club/books-list/src/app/services/interceptors/loader-interceptor.service.ts
@@ -6,16 +6,23 @@ import {
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { delay, finalize } from 'rxjs/operators';
+import { LoaderService } from '../loader.service';
 
 @Injectable()
 export class LoaderInterceptorService implements HttpInterceptor {
-  constructor() {}
+  constructor(private loader: LoaderService) {}
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     console.log('entrou no loader-interceptor');
+    this.loader.startLoading();
 
-    return next.handle(req);
+    return next.handle(req).pipe(
+      delay(3000), //useful to test slowness
+      finalize(() => this.loader.stopLoading())
+    );
   }
 }
