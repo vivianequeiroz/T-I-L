f908e4e73e4001ea30f5208a824b799e761273bb
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-12-19 18:18:51 -0300

    feat: customize how user see and interact with embedded map

--- angular-google-maps-experts-club/src/app/features/students/student-map/student-map.component.ts
+++ angular-google-maps-experts-club/src/app/features/students/student-map/student-map.component.ts
@@ -1,15 +1,30 @@
+import { StudentsService } from './../students.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-student-map',
   templateUrl: './student-map.component.html',
-  styleUrls: ['./student-map.component.css']
+  styleUrls: ['./student-map.component.css'],
 })
 export class StudentMapComponent implements OnInit {
+  mapOptions: google.maps.MapOptions = {
+    mapTypeId: 'roadmap',
+    mapTypeControl: false,
+    fullscreenControl: false,
+    zoom: 13,
+    maxZoom: 16,
+    minZoom: 12,
+    center: {
+      lat: -23.5489,
+      lng: -46.6388,
+    },
+  };
 
-  constructor() { }
+  constructor(private studentService: StudentsService) {}
 
   ngOnInit(): void {
+    this.loadMap();
   }
 
+  private loadMap() {}
 }
