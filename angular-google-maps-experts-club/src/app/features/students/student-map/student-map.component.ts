0aae6cbea503d6fafaf2848dc97ba34c48470ab9
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-12-19 18:45:49 -0300

    feat: add markers on the map for students already created

--- angular-google-maps-experts-club/src/app/features/students/student-map/student-map.component.ts
+++ angular-google-maps-experts-club/src/app/features/students/student-map/student-map.component.ts
@@ -1,5 +1,7 @@
 import { StudentsService } from './../students.service';
 import { Component, OnInit } from '@angular/core';
+import { Marker } from './Marker';
+import { Student } from '../student';
 
 @Component({
   selector: 'app-student-map',
@@ -7,6 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-map.component.css'],
 })
 export class StudentMapComponent implements OnInit {
+  markers: Marker[] = [];
+
+  mapCenter = {
+    lat: -22.935142495916043,
+    lng: -43.19813548085938,
+  };
+
   mapOptions: google.maps.MapOptions = {
     mapTypeId: 'roadmap',
     mapTypeControl: false,
@@ -14,10 +23,7 @@ export class StudentMapComponent implements OnInit {
     zoom: 13,
     maxZoom: 16,
     minZoom: 12,
-    center: {
-      lat: -23.5489,
-      lng: -46.6388,
-    },
+    center: this.mapCenter,
   };
 
   constructor(private studentService: StudentsService) {}
@@ -26,5 +32,39 @@ export class StudentMapComponent implements OnInit {
     this.loadMap();
   }
 
-  private loadMap() {}
+  private loadMap() {
+    this.studentService.findAll().subscribe((response) => {
+      response.forEach((student) => {
+        this.addMarker(student);
+      });
+    });
+  }
+
+  private addMarker(student: Student) {
+    const hasLatAndLng = student.lat && student.lng;
+    const marker: Marker = {
+      student,
+      options: {
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        icon: hasLatAndLng
+          ? undefined
+          : {
+              url: '/assets/icons/map-marker-alt-solid.svg',
+              scaledSize: new google.maps.Size(35, 35),
+            },
+        label: {
+          text: student.name,
+          fontWeight: 'bold',
+          color: hasLatAndLng ? '#000000' : '#FFFFFF',
+        },
+        position: {
+          lat: student.lat || this.mapCenter.lat,
+          lng: student.lng || this.mapCenter.lat,
+        },
+      },
+    };
+
+    this.markers.push(marker);
+  }
 }
