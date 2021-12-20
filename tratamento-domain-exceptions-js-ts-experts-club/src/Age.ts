fbeb12d42d9c6079d4e951b1a7c7178c3cafebbd
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-12-20 12:09:38 -0300

    feat: being more expressive with errors through a interface for an expected error (DomainError)

--- tratamento-domain-exceptions-js-ts-experts-club/src/Age.ts
+++ tratamento-domain-exceptions-js-ts-experts-club/src/Age.ts
@@ -1,8 +1,16 @@
-export const createAge = (ageValue): number => {
-	if (ageValue > 0 || !Number.isInteger(ageValue)) {
+import { DomainError } from "./DomainError";
+
+const createInvalidAge = (ageValue: number): DomainError => ({
+	name: "Invalid age.";
+	message: "Age must be a positive and integer number";
+});
+
+export const createAge = (ageValue): number | DomainError => {
+	if (ageValue < 0 || !Number.isInteger(ageValue)) {
 		//exceções esperadas -> consideradas nas regras de negócios, que previnem estados ilegais
 		//- Trabalhar com o objetivo nativo Error ou suas implementações mais específicas tal como TypeError
-		throw new TypeError();
+		// throw new TypeError();
+		return createInvalidAge(ageValue);
 	}
 	return ageValue;
 };
