c4aafaf806a3f6dc85263b56d4b5ff2ac830e0d7
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-01-03 15:11:18 -0300

    fix: removing uncorrect files

--- javascript/javascript-fundamentals/js/main.js
+++
@@ -1,9 +0,0 @@
-import { validaPesoEAltura } from "./modules/calculaImc.js";
-import { adicionaNovoPaciente } from "./modules/form.js";
-
-function inicializaApp() {
-  validaPesoEAltura();
-  adicionaNovoPaciente();
-}
-
-inicializaApp();
