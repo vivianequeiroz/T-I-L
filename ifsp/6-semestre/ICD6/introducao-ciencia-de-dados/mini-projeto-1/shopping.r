bb8a89096e028d29d1dc199eeb8f0dd72bac607d
Viviane Queiroz <vivianedsqueiroz@gmail.com>
2022-08-16 13:31:28 -0300

    wip: intro to exploratory analysis of csv file

---
+++ ifsp/6-semestre/ICD6/introducao-ciencia-de-dados/mini-projeto-1/shopping.r
@@ -0,0 +1,15 @@
+library(Hmisc)
+
+shoppingData <- read.csv2("shopping.csv")
+
+# Podemos obter, com a função summary, informações que descrevem o conjunto de dados e
+# que irão compor a visualização do boxplot dele:
+summary(shoppingData)
+
+# Com a função describe do pacote Hmisc, cada variável possui um retorno detalhado sobre suas características
+# e que compõem outras visualizações como no Diagrama de Frequências, por exemplo:
+describe(shoppingData)
+
+
+boxplot(shoppingData)
+
